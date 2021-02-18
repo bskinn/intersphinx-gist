fc95f026168ee17b09e942bf112019e83d52aabc
Brian Skinn <brian.skinn@gmail.com>
2021-02-18 07:12:47 -0500

    Set up repo and implement intersphinx gist test
    Now just need to flesh out the gist with any major non-RtD docs,
    alphabetize, and then set up the GHA.
    
    Keepalive commit could just be a report of the test results?

---
+++ test_isphx_gist.py
@@ -0,0 +1,72 @@
+r"""*Module to test link validity in intersphinx mappings gist.*
+
+**Author**
+    Brian Skinn (bskinn@alum.mit.edu)
+
+**File Created**
+    18 Feb 2021
+
+**Copyright**
+    \(c) Brian Skinn 2021
+
+**Source Repository**
+    http://www.github.com/bskinn/intersphinx-gist
+
+**Documentation**
+    N/A
+
+**License**
+    The MIT License; see |license_txt|_ for full license terms
+
+**Members**
+
+"""
+
+import re
+
+import pytest
+import requests as rq
+
+pat_domain = re.compile(r"https?://([^/]+)/")
+
+
+def get_mapping_tuples():
+    """Retrieve intersphinx gist and extract mapping tuples."""
+    pat_line = re.compile(r"<td[\s\S]+?js-file-line[\s\S]+?>[^<]+?http[^<]+</td>")
+    pat_tuple = re.compile(r"[(][^)]+[)]")
+
+    resp = rq.get("https://gist.github.com/bskinn/0e164963428d4b51017cebdb6cda5209")
+    data = resp.content.decode()
+    mchs = list(pat_line.finditer(data))
+
+    submchs = [
+        sm.group(0).replace("&#39;", "'")
+        for sm in [pat_tuple.search(m.group(0)) for m in mchs]
+    ]
+
+    return [eval(sm) for sm in submchs]
+
+
+mapping_tuples_list = get_mapping_tuples()
+mapping_tuple_ids = [pat_domain.search(t[0]).group(1) for t in mapping_tuples_list]
+
+
+@pytest.fixture(scope="session", params=mapping_tuples_list, ids=mapping_tuple_ids)
+def mapping_tuple(request):
+    """Supply the mapping tuples singly to a test function."""
+    return request.param
+
+
+def test_mapping_root(mapping_tuple):
+    """Check docs root link validity."""
+    assert rq.get(mapping_tuple[0]).ok
+
+
+def test_mapping_objects_inv(mapping_tuple):
+    """Check docs objects.inv link validity."""
+    inv_link = (
+        mapping_tuple[1]
+        if mapping_tuple[1]
+        else (mapping_tuple[0].removesuffix("/") + "/objects.inv")
+    )
+    assert rq.get(inv_link).ok
